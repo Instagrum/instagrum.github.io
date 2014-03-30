e8b8708f95bde36f1c4d460b51659f80b5c87721
arj1025 <aj.johnson202@gmail.com>
2014-03-30 12:12:09 -0700

    test

---
+++ counter.js
@@ -0,0 +1,19 @@
+/***********************************************************\
+  MegaCounter v1.1
+  A "configurable" hit counter for the rest of us...
+  by Joe Barta  http://www.pagetutor.com/megacounter/
+\***********************************************************/
+text_string = "likes";
+txt_color = "#3f729b"  // Text string color
+counter = 0;      // Counter starts at this,
+revision = 2;          // Every time you alter your initial counter value (above), you must also change the revision number.
+page_code = 1;         // Unless you want every page to use the same counter, you'll need to use a different page code for each page.
+hitsperday = 68400;     // How many hits per day you want.
+bg_color = "#ffffff"   // Background color of counter
+fg_color = "#3f729b"   // Foreground color of counter
+link_color = "#0000FF" // Color of link back (Powered by MegaCounter)
+back_color = "#FFFFFF" // Color of white space in counter area
+//--></SCRIPT>
+<SCRIPT language="JavaScript" SRC="http://www.pagetutor.com/megacounter/generic_cookie_1yr.js"></SCRIPT>
+<SCRIPT language="JavaScript" SRC="http://www.pagetutor.com/megacounter/megacounter.js"></SCRIPT>
+<!-- End MegaCounter HEAD tag code -->
