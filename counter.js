02e2b49c08a8724495f57c3fd4aaaa961b5e4cd8
arj1025 <aj.johnson202@gmail.com>
2014-03-30 19:05:34 -0700

    v1.1
    cleaned up code and cleaned up page

--- counter.js
+++ counter.js
@@ -4,13 +4,13 @@
   by Joe Barta  http://www.pagetutor.com/megacounter/
 \***********************************************************/
 text_string = "likes";
-txt_color = "#3f729b"  // Text string color
-counter = 0;      // Counter starts at this,
-revision = 3;          // Every time you alter your initial counter value (above), you must also change the revision number.
-page_code = 1;         // Unless you want every page to use the same counter, you'll need to use a different page code for each page.
-hitsperday = 2039736;     // How many hits per day you want.
-bg_color = "#ffffff"   // Background color of counter
-fg_color = "#3f729b"   // Foreground color of counter
+txt_color = "#3f729b" // Text string color
+counter = 0; // Counter starts at this,
+revision = 3; // Every time you alter your initial counter value (above), you must also change the revision number.
+page_code = 1; // Unless you want every page to use the same counter, you'll need to use a different page code for each page.
+hitsperday = 2039736; // How many hits per day you want.
+bg_color = "#ffffff" // Background color of counter
+fg_color = "#3f729b" // Foreground color of counter
 link_color = "#0000FF" // Color of link back (Powered by MegaCounter)
 back_color = "#FFFFFF" // Color of white space in counter area
 //--></SCRIPT>
