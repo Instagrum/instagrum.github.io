86ac155e8106bd9788f1da48436cb456799a7cf7
arj1025 <aj.johnson202@gmail.com>
2014-04-01 16:06:13 -0700

    v1.3

--- counter.js
+++ counter.js
@@ -1,16 +1,28 @@
-/***********************************************************\
-  MegaCounter v1.1
-  A "configurable" hit counter for the rest of us...
-  by Joe Barta  http://www.pagetutor.com/megacounter/
-\***********************************************************/
-text_string = "likes";
-txt_color = "#3f729b" // Text string color
-counter = 0; // Counter starts at this,
-revision = 3; // Every time you alter your initial counter value (above), you must also change the revision number.
-page_code = 1; // Unless you want every page to use the same counter, you'll need to use a different page code for each page.
-hitsperday = 2039736; // How many hits per day you want.
-bg_color = "#ffffff" // Background color of counter
-fg_color = "#3f729b" // Foreground color of counter
-link_color = "#0000FF" // Color of link back (Powered by MegaCounter)
-back_color = "#FFFFFF" // Color of white space in counter area
-//--></SCRIPT>
+
+var START_DATE = new Date("March 25, 2014 00:00:00"); // put in the starting date here
+var INTERVAL = 1800; // in seconds
+var INCREMENT = 603; // increase per tick
+var START_VALUE = 0; // initial value when it's the start date
+var count = 0;
+
+window.onload = function()
+{
+var msInterval = INTERVAL * 1000;
+var now = new Date();
+count = parseInt((now - START_DATE)/msInterval) * INCREMENT + START_VALUE;
+document.getElementById('counter').innerHTML = count;
+setInterval("count += INCREMENT; document.getElementById('counter').innerHTML = count;", msInterval);
+}
+
+function addCommas(nStr)
+{
+nStr += '';
+x = nStr.split('.');
+x1 = x[0];
+x2 = x.length > 1 ? '.' + x[1] : '';
+var rgx = /(\d+)(\d{3})/;
+while (rgx.test(x1)) {
+x1 = x1.replace(rgx, '$1' + ',' + '$2');
+}
+return x1 + x2;
+}
